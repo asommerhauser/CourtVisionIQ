2f9bbe5d5fabbcc72dfe5390e6e6f84b62e6a921
Alec Sommerhauser <alecjsommerhauser@gmail.com>
2026-01-20 14:01:05 -0800

    Created preprocessor in event-time-model

--- models/event_time_model.py
+++ models/event_time_model.py
@@ -1,16 +1,33 @@
 import tensorflow as tf
 from keras import layers, models, Input
 from config import MAX_SEQUENCE_LENGTH
+from pathlib import Path
 
 class EventTimeModel:
     """
     The event time model - more to come.
     """
 
-    def __init__(self, max_seq_len=MAX_SEQUENCE_LENGTH, model_dim=256, num_event_classes=7, encoder):
+    def __init__(self, max_seq_len=MAX_SEQUENCE_LENGTH, model_dim=256, num_event_classes=7, encoder, path="./data"):
         self.max_seq_len = max_seq_len
         self.model_dim = model_dim
         self.num_event_classes = num_event_classes
+        self.data_dir = Path(path)
+        if not self.data_dir.exists():
+            raise FileNotFoundError(f"Data directory not found: {self.data_dir.resolve()}")
+        self.csv_files = list(self.data_dir.glob("*.csv"))
+        self.encoder = encoder
+
+    def preprocess(self):
+        for csv_path in self.csv_files:
+            print(f"Processing {csv_path.name}")
+            df = pd.read_csv(csv_path)
+
+            df = df[["teammates", "opponents"]]
+
+            df["teammates_encoded"] = df["teammates"].apply(self.encoder.encode_roster)
+            df["opponents_encoded"] = df["opponents"].apply(self.encoder.encode_roster)
+            print(df)
 
     def model(self):
         """
